c14ffb88acf0f3df48955852fe064fe8448aa6bf
Shrutika <shruti.ghosarawade@gmail.com>
2025-10-26 19:37:14 +0530

    Add Project Files

--- app.js
+++ app.js
@@ -80,6 +80,10 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.get("/", (req, res) => {
+  res.render("home"); // your home.ejs layout
+});
+
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
--- routes/review.js
+++ routes/review.js
@@ -4,7 +4,7 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const ExpressError=require("../utils/ExpressError.js");
 const Review=require("../models/review.js");
 const Listing=require("../models/listing.js");
-const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
+const {validateReview, isLoggedIn, isReviewAuthor}=require("../middleware.js");
 
 const reviewController=require("../controllers/reviews.js");
 
@@ -14,6 +14,6 @@ router.post("/",isLoggedIn,validateReview,wrapAsync(reviewController.createRevie
 );
 
 //delete review Route
-router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(reviewController.destroyReview));
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(reviewController.destroyReview));
 
 module.exports=router;
